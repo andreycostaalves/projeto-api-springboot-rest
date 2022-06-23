495d87d72a4ed171c3665bafc78af99deaece089
Andrey Alves <Andreyalvesgit@gmail.com>
2022-06-23 17:00:05 -0300

    JWT: Criando service e validações

---
+++ src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,84 @@
+package curso.api.rest.security;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import projeto.spring.api.projetospringapi.ApplicationContextLoad;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+    /* tempo de validade do token */
+    private static final long EXPIRATION_TIME = 172800000; /* 2 dias */
+
+    /* Senha única para compor a autenticação*/
+    private static final String SECRET = "SenhaExtremamenteSecreta";
+
+    /* Prefixo padrão de token */
+    private static final String TOKEN_PREFIX = "Bearer";
+
+
+    private static final  String HEADER_STRING ="Authorization";
+
+
+    /* Gerando token de autenticação e adicionando ao cabeçalho e resposta Http */
+    public void addAuthentication(HttpServletResponse response, String username )throws Exception{
+
+        /*Montagem do token */
+        String JWT = Jwts.builder()  /*Chama o gerador de token*/
+                .setSubject(username)  /* Adiciona o usuario*/
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) /*Tempo de Expiração*/
+                .signWith(SignatureAlgorithm.HS512, SECRET).compact(); /* Compactação e algoritimo de geração de senha.*/
+
+        /* JUNTA O TOKEN COM O PREFIXO.*/
+        String token = TOKEN_PREFIX + " " + JWT;
+
+        /* Adiciona no cabeçalho http */
+        response.addHeader(HEADER_STRING, token);
+
+        /* Escrevendo  token como resposta no corpo http*/
+        response.getWriter().write("{\"Authorization\""+token+"\"}");
+
+    }
+
+    /*Retorna o usuario validado com o token ou caso não seja validade retorna null */
+    public Authentication getAuthentication(HttpServletRequest request){
+
+        /*Pega o token enviado no cabeçalho http*/
+        String token = request.getHeader(HEADER_STRING);
+
+        if(token != null){
+            /*Faz a validação do token do usuario na requisição.*/
+            String user =Jwts.parser().setSigningKey(SECRET)
+                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))     //retira o token prefix do body
+                    .getBody().getSubject();
+            if(user != null){
+                Usuario usuario  = ApplicationContextLoad.getApplicationContext()
+                        .getBean(UsuarioRepository.class).findUserByLogin(user);
+                /*Retorna o usuario logado.*/
+
+                if(usuario != null){
+                    return new UsernamePasswordAuthenticationToken(usuario.getLogin(),
+                            usuario.getSenha(),
+                            usuario.getAuthorities());
+                }
+            }
+        }
+
+        return null; //Não autorizado
+
+
+    }
+
+
+
+}
---
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,55 @@
+package curso.api.rest.security;
+
+import curso.api.rest.service.ImplementacaoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+/*Mapear URL, endereços, autoriza ou bloqueia acessos a url´s.*/
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+    //Configura as solicitações de acesso por Http
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /* Ativando a proteção contra usuario que não estão validados por token */
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+        /* Ativando a permissão para acesso a pagina inicial do sistema: EX: sistema.com/index */
+                .disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+        /* URL DE LOGOUT*: Redireciona após o usuario deslogar do sistema. */
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+        /* Mapeia URL de logout e invalida o usuario. */
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
+        /* Filtra requisições de login para autenticação */
+
+        /* Filtra demais requisições para verificar a presença do token JWT no Header http  */
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //service que irá consultar usuario no banco de dados.
+        auth.userDetailsService(implementacaoUserDetailsService)
+                //Padrão de codificação de senhas
+                .passwordEncoder(new BCryptPasswordEncoder());
+
+
+    }
+}
